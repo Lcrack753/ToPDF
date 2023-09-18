72bb14745dd48931b868bb7c4d236022fe91ac55
Lucio Moro <lcarnero@jst.gob.ar>
2023-09-18 15:56:28 -0300

    Excel agregado

--- ClassToPDF/word.py
+++ ClassToPDF/word.py
@@ -1,35 +1,85 @@
 import win32com.client
-import re
 import os
 
-class FileWord():
+class FileToPDF():
 
-    def __init__(self, origen,destino):
-        self.origen= origen.replace('\\', '/')
-        self.destino = destino.replace('\\', '/') + '/'
-        self.nombre = self.origen.split('/')[-1].split('.')[0]
+    def __init__(self, origen, destino):
+        try:
+            self.origen = os.path.normpath(origen)
+            self.destino = os.path.normpath(destino)
+        except Exception as e:
+            print('Ingrese una ruta valida')
 
     def word_to_pdf(self):
+        if len(lista_docx(self.origen)) == 0:
+            print(f'No hay archivos .docx en el directorio {self.origen}')
+            return 
         try:
             # Crear una instancia de Word
             word = win32com.client.Dispatch('Word.Application')
+            docx_n = 0
+            word.Visible = False
+            for file in lista_docx(self.origen):
+                # Saltea archivo temporal
+                if file.startswith("~$"):
+                    continue  
 
-            # Abrir el archivo de Word
-            doc = word.Documents.Open(self.origen)
+                docs_name = file.split('.')[0].replace(' ', '_')
+                
+                # Abrir el archivo de Word
+                file_path = os.path.join(self.origen, file)
+                doc = word.Documents.Open(file_path)
 
-            # Guardar el archivo como PDF
-            doc.SaveAs(self.destino + self.nombre + '.pdf', FileFormat=17)  # 17 es el formato para PDF
+                # Guardar el archivo como PDF
+                pdf_path = os.path.join(self.destino, docs_name + '.pdf')
+                doc.SaveAs(pdf_path, FileFormat=17)  # 17 es el formato para PDF
 
-            # Cerrar el archivo de Word
-            doc.Close()
+                # Cerrar el archivo de Word
+                doc.Close()
+                docx_n = docx_n + 1
 
             # Salir de Word
             word.Quit()
-
-            print(f"El archivo {self.nombre} se ha convertido a {self.nombre}.pdf exitosamente.")
+            print(f'Se realizo con exito la conversion de {docx_n} archivos .docx')
         except Exception as e:
             print(f"Se produjo un error: {str(e)}")
 
+    def excel_to_pdf(self):
+        if len(lista_xlsx(self.origen)) == 0:
+            print(f'No hay archivos .xlsx en el directorio {self.origen}')
+            return
+        try:
+            # Crear una instancia de Excel
+            excel = win32com.client.Dispatch('Excel.Application')
+            excel.Visible = False  # Evitar que Excel sea visible durante la conversión
+
+            for file in lista_xlsx(self.origen):
+                # Saltea archivo temporal
+                if file.startswith("~$"):
+                    continue
+
+                excel_n = 0
+
+                excel_file_name = file.split('.')[0].replace(' ', '_')
+
+                # Abrir el archivo de Excel
+                file_path = os.path.join(self.origen, file)
+                workbook = excel.Workbooks.Open(file_path)
+
+                # Guardar el archivo como PDF
+                pdf_path = os.path.join(self.destino, excel_file_name + '.pdf')
+                workbook.ExportAsFixedFormat(0, pdf_path)  # 0 es el formato para PDF
+
+                # Cerrar el archivo de Excel
+                workbook.Close()
+
+                excel_n = excel_n + 1
+
+            # Salir de Excel
+            excel.Quit()
+            print(f'Se realizo con exito la conversion de {excel_n} archivos .xlsx')
+        except Exception as e:
+            print(f"Se produjo un error: {str(e)}")
 
 def es_docx(file):
     return file.split('.')[-1] == 'docx'
@@ -47,6 +97,6 @@ def es_xlsx(file):
 def lista_xlsx(dir):
     lista_xlsx = []
     for file in os.listdir(dir):
-        if es_xlsx(file):
+        if es_xlsx(file) and not file.startswith("~$"): # Saltea archivo temporal
             lista_xlsx.append(file)
     return lista_xlsx
--- application.py
+++ application.py
@@ -1,18 +1,18 @@
-import os
 from ClassToPDF.word import *
 
-dir_actual = input("Escriba el directorio contenedor de los archivos docx: ")
-
-if dir_actual.strip()[-1] != '/':
-    dir_actual = dir_actual + '/'
+dir_actual = input("Escriba el directorio: ")
 
 if not os.path.exists(dir_actual + '/pdfs'):
     os.mkdir(dir_actual + '/pdfs')
 
+
 dir_destino = dir_actual + '/pdfs'
+direc_origen= FileToPDF(dir_actual, dir_destino)
 
-lista_de_docs = lista_docx(os.listdir(dir_actual))
-
-for archivo in lista_de_docs:
-    archivo = FileWord(dir_actual + archivo, dir_destino)
-    archivo.word_to_pdf()
+n = int(input('escriba 1 para convertir documentos .docx\nEscriba 2 para convertir documentos .xlxs: '))
+if n == 1:
+    direc_origen.word_to_pdf()
+elif n == 2:
+    direc_origen.excel_to_pdf()
+else:
+    print('ERROR inesperado')
