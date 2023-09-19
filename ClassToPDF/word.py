a877f8903942e25078feff9531524d8e8335d52e
Lucio Moro <lcarnero@jst.gob.ar>
2023-09-19 10:51:14 -0300

    Compile PDFs

--- ClassToPDF/word.py
+++ ClassToPDF/word.py
@@ -1,6 +1,6 @@
 import win32com.client
 import os
-
+import fitz
 class FileToPDF():
 
     def __init__(self, origen, destino):
@@ -9,9 +9,12 @@ class FileToPDF():
             self.destino = os.path.normpath(destino)
         except Exception as e:
             print('Ingrese una ruta valida')
+        self.lista_docx = lista_docx(self.origen)
+        self.lista_xlsx = lista_xlsx(self.origen)
+        self.lista_pdf = lista_pdf(self.origen)
 
     def word_to_pdf(self):
-        if len(lista_docx(self.origen)) == 0:
+        if len(self.lista_docx) == 0:
             print(f'No hay archivos .docx en el directorio {self.origen}')
             return 
         try:
@@ -19,11 +22,7 @@ class FileToPDF():
             word = win32com.client.Dispatch('Word.Application')
             docx_n = 0
             word.Visible = False
-            for file in lista_docx(self.origen):
-                # Saltea archivo temporal
-                if file.startswith("~$"):
-                    continue  
-
+            for file in self.lista_docx:
                 docs_name = file.split('.')[0].replace(' ', '_')
                 
                 # Abrir el archivo de Word
@@ -45,20 +44,16 @@ class FileToPDF():
             print(f"Se produjo un error: {str(e)}")
 
     def excel_to_pdf(self):
-        if len(lista_xlsx(self.origen)) == 0:
+        if len(self.lista_xlsx) == 0:
             print(f'No hay archivos .xlsx en el directorio {self.origen}')
             return
         try:
             # Crear una instancia de Excel
             excel = win32com.client.Dispatch('Excel.Application')
             excel.Visible = False  # Evitar que Excel sea visible durante la conversión
+            excel_n = 0
 
-            for file in lista_xlsx(self.origen):
-                # Saltea archivo temporal
-                if file.startswith("~$"):
-                    continue
-
-                excel_n = 0
+            for file in self.lista_xlsx:
 
                 excel_file_name = file.split('.')[0].replace(' ', '_')
 
@@ -81,6 +76,24 @@ class FileToPDF():
         except Exception as e:
             print(f"Se produjo un error: {str(e)}")
 
+    def compile(self, lista_pdf, origen):
+        if len(lista_pdf) == 0:
+            print (f'No hay archivos .pdf en {origen}')
+            return
+        
+        result = fitz.open()
+
+        for pdf in lista_pdf:
+            with fitz.open(os.path.join(origen, pdf)) as mfile:
+                result.insert_pdf(mfile)
+            
+        result.save(os.path.join(self.destino, 'resultado.pdf'))
+
+        print(f'Se unieron los siguientes pdf en {os.path.join(self.destino, "resultado.pdf")}:')
+        for pdf in lista_pdf: print(pdf)
+
+
+
 def es_docx(file):
     return file.split('.')[-1] == 'docx'
 
@@ -100,3 +113,14 @@ def lista_xlsx(dir):
         if es_xlsx(file) and not file.startswith("~$"): # Saltea archivo temporal
             lista_xlsx.append(file)
     return lista_xlsx
+
+def es_pdf(file):
+    return file.split('.')[-1] == 'pdf'
+
+def lista_pdf(dir):
+    lista_pdf = []
+    for file in os.listdir(dir):
+        if es_pdf(file):
+            lista_pdf.append(file)
+    return lista_pdf
+
