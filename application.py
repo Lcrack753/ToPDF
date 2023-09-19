0cf59592de6eb06b1ed1613995926d00795e68d0
Lucio Moro <lcarnero@jst.gob.ar>
2023-09-19 14:41:55 -0300

    Primer cambio de la consola

--- application.py
+++ application.py
@@ -4,7 +4,47 @@ dir_actual = input("Escriba el directorio: ")
 
 if not os.path.exists(dir_actual + '/pdfs'):
     os.mkdir(dir_actual + '/pdfs')
+        
+dir_destino = dir_actual + '/pdfs'
+dir = FileToPDF(dir_actual, dir_destino)
 
+while True:
+    print('Convertir archivos [1]')
+    print('Combinar PDFs [2]')
+    print('Lista de archivos [3]')
+    accion = int(input('>>> '))
+    
+
+    if accion == 1:
+        print('Archivos Word [1]')
+        print('Archivos Excel [2]')
+        tipo = int(input('>>> '))
+        
+        if tipo == 1:
+            dir.word_to_pdf()
+            dir.compile(dir.destino)
+
+        if tipo == 2:
+            dir.excel_to_pdf()
+            dir.compile(dir.destino)
+    
+    if accion == 2:
+        dir.compile(dir.origen)
+    
+    if accion == 3:
+        print('archivos word')
+        for file in dir.lista_docx: print(file)
+        print('archivos excel')
+        for file in dir.lista_xlsx: print(file)
+        print('archivos pdfs')
+        for file in dir.lista_pdf: print(file)
+    
+    print('desea realizar otra accion?')
+    print('Si [1]')
+    print('No [2]')
+    x = int(input('>>> '))
+    if x == 1:
+        continue
 
 dir_destino = dir_actual + '/pdfs'
 direc_origen= FileToPDF(dir_actual, dir_destino)
